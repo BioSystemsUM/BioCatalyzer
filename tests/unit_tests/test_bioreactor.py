d8115f8bc22035d8d2117af2547d7cb69bc6be8c
João Correia <jfscorreia95@gmail.com>
2022-09-22 10:55:25 +0100

    [ADD] test bioreactor

---
+++ tests/unit_tests/test_bioreactor.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+from unittest import TestCase
+
+from biocatalyzer.bioreactor import BioReactor
+
+
+class TestCompound(TestCase):
+
+    def setUp(self):
+        self.output_folder = 'results/'
+        os.mkdir(self.output_folder)
+
+    def tearDown(self):
+        if os.path.exists(self.output_folder):
+            shutil.rmtree(self.output_folder)
+
+    def test_bioreactor(self):
+        br = BioReactor(compounds_path='data/compounds_sample/compounds.tsv',
+                        reaction_rules_path='data/reaction_rules_sample/reactionrules.tsv',
+                        coreactants_path='data/coreactants_sample/coreactants.tsv',
+                        patterns_to_remove_path='data/patterns_to_remove_sample/patterns.tsv',
+                        molecules_to_remove_path='data/byproducts_to_remove_sample/byproducts.tsv',
+                        output_path=self.output_folder,
+                        n_jobs=12)
+        br.react()
