0034a8c99b201bcffe91d726c07fe567425727b3
João Correia <jfscorreia95@gmail.com>
2022-09-27 13:19:18 +0100

    [REFACTOR] change data dir

--- tests/unit_tests/test_bioreactor.py
+++ tests/unit_tests/test_bioreactor.py
@@ -20,10 +20,10 @@ class TestBioReactor(BioReactorTestCase, TestCase):
 
     def test_bioreactor(self):
         br = BioReactor(compounds_path='tests/data/compounds_sample/compounds.tsv',
-                        reaction_rules_path='data/reaction_rules_sample/reactionrules.tsv',
-                        coreactants_path='data/coreactants_sample/coreactants.tsv',
-                        patterns_to_remove_path='data/patterns_to_remove_sample/patterns.tsv',
-                        molecules_to_remove_path='data/byproducts_to_remove_sample/byproducts.tsv',
+                        reaction_rules_path='tests/data/reaction_rules_sample/reactionrules.tsv',
+                        coreactants_path='tests/data/coreactants_sample/coreactants.tsv',
+                        patterns_to_remove_path='tests/data/patterns_to_remove_sample/patterns.tsv',
+                        molecules_to_remove_path='tests/data/byproducts_to_remove_sample/byproducts.tsv',
                         output_path=self.output_folder,
                         n_jobs=12)
         br.react()
