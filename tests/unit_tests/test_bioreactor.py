f4c68dba633d26c1955157cb73ffb62a034f171d
João Correia <jfscorreia95@gmail.com>
2022-09-27 12:53:47 +0100

    [REMOVE] MockBioReactor

--- tests/unit_tests/test_bioreactor.py
+++ tests/unit_tests/test_bioreactor.py
@@ -2,7 +2,7 @@ import os
 import shutil
 from unittest import TestCase
 
-from mock_bioreactor import MockBioReactor
+from biocatalyzer.bioreactor import BioReactor
 
 
 class BioReactorTestCase(TestCase):
@@ -19,11 +19,11 @@ class BioReactorTestCase(TestCase):
 class TestBioReactor(BioReactorTestCase, TestCase):
 
     def test_bioreactor(self):
-        br = MockBioReactor(compounds_path='data/compounds_sample/compounds.tsv',
-                            reaction_rules_path='data/reaction_rules_sample/reactionrules.tsv',
-                            coreactants_path='data/coreactants_sample/coreactants.tsv',
-                            patterns_to_remove_path='data/patterns_to_remove_sample/patterns.tsv',
-                            molecules_to_remove_path='data/byproducts_to_remove_sample/byproducts.tsv',
-                            output_path=self.output_folder,
-                            n_jobs=12)
+        br = BioReactor(compounds_path='data/compounds_sample/compounds.tsv',
+                        reaction_rules_path='data/reaction_rules_sample/reactionrules.tsv',
+                        coreactants_path='data/coreactants_sample/coreactants.tsv',
+                        patterns_to_remove_path='data/patterns_to_remove_sample/patterns.tsv',
+                        molecules_to_remove_path='data/byproducts_to_remove_sample/byproducts.tsv',
+                        output_path=self.output_folder,
+                        n_jobs=12)
         br.react()
