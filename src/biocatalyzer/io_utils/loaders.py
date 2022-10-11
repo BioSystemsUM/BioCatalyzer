6e23ea6544f76e4ae5cb467b8e720d7d79cee984
João Correia <jfscorreia95@gmail.com>
2022-10-11 08:24:32 +0100

    [REFACTOR] add rules and coreactants path as args again

--- src/biocatalyzer/io_utils/loaders.py
+++ src/biocatalyzer/io_utils/loaders.py
@@ -30,18 +30,34 @@ class Loaders:
         return compounds[['compound_id', 'smiles']]
 
     @staticmethod
-    def load_reaction_rules(orgs='ALL'):
+    def load_reaction_rules(path='data/reactionrules/all_reaction_rules_forward_no_smarts_duplicates.tsv', orgs='ALL'):
         """
         Load the reaction rules to use.
+
+        Parameters
+        ----------
+        path: str
+            Path to the reaction rules.
+        orgs: Union[list, str]
+            List of organisms to use. If 'ALL', all organisms will be used.
 
         Returns
         -------
         pd.DataFrame:
             pandas dataframe with the reaction rules to use.
         """
-        rules = pd.read_csv('data/reactionrules/all_reaction_rules_forward_no_smarts_duplicates.tsv',
-                            header=0,
-                            sep='\t')
+        rules = pd.read_csv(path, header=0, sep='\t')
+
+        if 'InternalID' not in rules.columns:
+            raise ValueError('The reaction rules file must contain a column named "InternalID".')
+        if 'Reactants' not in rules.columns:
+            raise ValueError('The reaction rules file must contain a column named "Reactants".')
+        if 'SMARTS' not in rules.columns:
+            raise ValueError('The reaction rules file must contain a column named "SMARTS".')
+        if 'EC_Numbers' not in rules.columns:
+            raise ValueError('The reaction rules file must contain a column named "EC_Numbers".')
+        if 'Organisms' not in rules.columns:
+            raise ValueError('The reaction rules file must contain a column named "Organisms".')
 
         def match_org(value, orgs_list):
             if isinstance(value, str):
@@ -50,8 +66,11 @@ class Loaders:
             return False
 
         if not isinstance(orgs, str):
+            # TODO: check if adding expontaneous reactions actually makes sense
+            orgs.append('expontaneous')
             rules['has_org'] = rules.apply(lambda x: match_org(x['Organisms'], orgs), axis=1)
             rules = rules[rules['has_org']]
+            rules.drop('has_org', axis=1, inplace=True)
         return rules
 
     @staticmethod
@@ -70,16 +89,26 @@ class Loaders:
         return orgs.org_id.values
 
     @staticmethod
-    def load_coreactants():
+    def load_coreactants(path='data/coreactants/all_coreactants.tsv'):
         """
         Load the coreactants to use.
+
+        Parameters
+        ----------
+        path: str
+            Path to the coreactants.
 
         Returns
         -------
         pd.DataFrame:
             pandas dataframe with the coreactants to use.
         """
-        return pd.read_csv('data/coreactants/all_coreactants.tsv', header=0, sep='\t')
+        coreactants = pd.read_csv(path, header=0, sep='\t')
+        if 'smiles' not in coreactants.columns:
+            raise ValueError('The compounds file must contain a column named "smiles".')
+        if 'compound_id' not in coreactants.columns:
+            raise ValueError('The compounds file must contain a column named "compound_id".')
+        return coreactants
 
     @staticmethod
     def load_byproducts_to_remove(path):
