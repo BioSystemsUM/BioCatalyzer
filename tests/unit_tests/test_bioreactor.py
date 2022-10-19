51c65583634b62e5e1d5928a3085b33e2fe1e9ae
João Correia <jfscorreia95@gmail.com>
2022-10-19 08:57:16 +0100

    [ADD] testing bioreactor properties and setters

--- tests/unit_tests/test_bioreactor.py
+++ tests/unit_tests/test_bioreactor.py
@@ -13,6 +13,7 @@ class BioReactorTestCase(TestCase):
 
     def setUp(self):
         self.output_folder = 'results/'
+        self.new_output_folder = 'new_output_path/'
         os.mkdir(self.output_folder)
 
     def tearDown(self):
@@ -65,10 +66,85 @@ class TestBioReactor(BioReactorTestCase, TestCase):
                                        patterns_to_remove_path=patterns_to_remove_path,
                                        molecules_to_remove_path=molecules_to_remove_path,
                                        output_path=self.output_folder,
-                                       n_jobs=12)
+                                       n_jobs=-1)
         br_no_orgs_filter.react()
 
         self.assertEqual(br_no_orgs_filter.reaction_rules.shape, (29297, 7))
         self.assertEqual(br_no_orgs_filter.compounds.shape, (3, 2))
         self.assertIsInstance(br_no_orgs_filter.new_compounds, pd.DataFrame)
         self.assertEqual(br_no_orgs_filter.new_compounds.shape[1], 7)
+
+    def test_bioreactor_properties_and_setters(self):
+        compounds_path = os.path.join(TESTS_DATA_PATH, 'compounds_sample/compounds.tsv')
+        organisms_path = os.path.join(TESTS_DATA_PATH, 'organisms_sample/organisms_to_use.tsv')
+        br = BioReactor(compounds_path=compounds_path,
+                        organisms_path=organisms_path,
+                        output_path=self.output_folder,
+                        n_jobs=12)
+
+        with self.assertRaises(ValueError):
+            _ = br.new_compounds
+        with self.assertRaises(AttributeError):
+            br.new_compounds = 'random_thing'
+
+        output_path = br.output_path
+        self.assertEqual(output_path, self.output_folder)
+
+        br.output_path = self.new_output_folder
+        shutil.rmtree(self.new_output_folder)
+
+        with self.assertRaises(FileExistsError):
+            br.output_path = os.path.join(TESTS_DATA_PATH, 'results_sample/')
+
+        br.react()
+
+        with self.assertRaises(FileNotFoundError):
+            br.compounds = 'not_existing_path.tsv'
+
+        br.compounds = os.path.join(TESTS_DATA_PATH, 'compounds_sample/compounds_subsample.tsv')
+
+        br.compounds = 'CN1C=NC2=C1C(=O)N(C(=O)N2C)C;C(C1C(C(C(C(O1)O)O)O)O)O'
+
+        with self.assertRaises(FileNotFoundError):
+            br.reaction_rules = 'not_existing_path.tsv'
+
+        br.reaction_rules = os.path.join(TESTS_DATA_PATH, 'reaction_rules_sample/reactionrules_subsample.tsv')
+
+        br.output_path = 'new_output_path'
+
+        _ = br.compounds_path
+        with self.assertRaises(FileNotFoundError):
+            br.compounds_path = 'not_existing_path.tsv'
+
+        br.compounds_path = os.path.join(TESTS_DATA_PATH, 'compounds_sample/compounds_subsample.tsv')
+
+        _ = br.neutralize
+        br.neutralize = True
+
+        _ = br.organisms_path
+        with self.assertRaises(FileNotFoundError):
+            br.organisms_path = 'not_existing_path.tsv'
+
+        br.organisms_path = os.path.join(TESTS_DATA_PATH, 'organisms_sample/organisms_subsample.tsv')
+
+        br.organisms_path = 'hsa;eco'
+
+        _ = br.molecules_to_remove_path
+        with self.assertRaises(FileNotFoundError):
+            br.molecules_to_remove_path = 'not_existing_path.tsv'
+
+        br.molecules_to_remove_path = os.path.join(TESTS_DATA_PATH,
+                                                   'byproducts_to_remove_sample/byproducts_subsample.tsv')
+
+        _ = br.patterns_to_remove_path
+        with self.assertRaises(FileNotFoundError):
+            br.patterns_to_remove_path = 'not_existing_path.tsv'
+
+        br.patterns_to_remove_path = os.path.join(TESTS_DATA_PATH, 'patterns_to_remove_sample/patterns_subsample.tsv')
+
+        mac = br.min_atom_count
+        br.min_atom_count = mac + 1
+
+        _ = br.n_jobs
+        br.n_jobs = -1
+
