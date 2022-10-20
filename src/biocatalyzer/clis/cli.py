4ac56cd9863a269cc8797477b06a2398051711b8
João Correia <jfscorreia95@gmail.com>
2022-10-20 10:49:10 +0100

    [FIX] allow organisms to be defined as a ;-separated string

--- src/biocatalyzer/clis/cli.py
+++ src/biocatalyzer/clis/cli.py
@@ -31,7 +31,7 @@ DATA_FILES = os.path.dirname(__file__)
               help="Path to reaction rules file.")
 @click.option("--organisms",
               "organisms",
-              type=click.Path(exists=True),
+              type=str,
               default=None,
               help="The path to the user defined file containing the organisms to filter the reaction rules.",
               )
@@ -113,6 +113,7 @@ def biocatalyzer_cli(compounds,
     if reaction_rules is None:
         reaction_rules = os.path.join(
             DATA_FILES, '../data/reactionrules/all_reaction_rules_forward_no_smarts_duplicates_sample.tsv')
+    logging.basicConfig(filename=f'{output_path}logging.log', level=logging.DEBUG)
     br = BioReactor(compounds_path=compounds,
                     output_path=output_path,
                     reaction_rules_path=reaction_rules,
@@ -122,7 +123,6 @@ def biocatalyzer_cli(compounds,
                     molecules_to_remove_path=molecules_to_remove,
                     min_atom_count=min_atom_count,
                     n_jobs=n_jobs)
-    logging.basicConfig(filename=f'{output_path}logging.log', level=logging.DEBUG)
     br.react()
 
     if match_ms_data:
