33fe85b337bbfbb2d40cf5a2ac8490681b796f2a
João Correia <jfscorreia95@gmail.com>
2022-10-20 10:46:22 +0100

    [ADD] CLI matcher implementation

--- src/biocatalyzer/clis/cli_matcher.py
+++ src/biocatalyzer/clis/cli_matcher.py
@@ -1,13 +1,61 @@
+import logging
+
 import click
+
+from biocatalyzer import MSDataMatcher
 
 
 @click.command()
-@click.argument("arg",
+@click.argument("ms_data",
                 type=str,
                 required=True,
                 )
-def matcher_cli(arg):
-    print(f"{arg}_2")
+@click.argument("compounds_to_match",
+                type=str,
+                required=True,
+                )
+@click.argument("output_path",
+                type=click.Path(),
+                required=True,
+                )
+@click.option("--mode",
+              "mode",
+              type=click.Choice(['mass', 'mass_diff']),
+              default='mass',
+              show_default=True,
+              help="The mode to use for the MS data matching (mass for ExactMass matching or mass_dif for ExactMass "
+                   "differences matching).",
+              )
+@click.option("--tolerance",
+              "tolerance",
+              type=float,
+              default=0.02,
+              show_default=True,
+              help="The mass tolerance to use when matching MS data.",
+              )
+def matcher_cli(ms_data,
+                compounds_to_match,
+                output_path,
+                mode,
+                tolerance):
+    """Run the MSDataMatcher.
+
+    Mandatory arguments:
+
+        ms_data: Path to the file containing the MS data to use.
+
+        compounds_to_match: Path to the file containing the compounds to match with the MS data.
+
+        output_path: Path to the output directory.
+    """
+    logging.basicConfig(filename=f'{output_path}logging.log', level=logging.DEBUG)
+    ms = MSDataMatcher(ms_data_path=ms_data,
+                       compounds_to_match=compounds_to_match,
+                       output_path=output_path,
+                       mode=mode,
+                       tolerance=tolerance)
+
+    ms.generate_ms_results()
 
 
 if __name__ == "__main__":
