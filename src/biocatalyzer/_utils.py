11986d581a8aa5b0bcee54e2edaba823811ce921
João Correia <jfscorreia95@gmail.com>
2022-10-24 13:15:35 +0100

    [REFACTOR] move _empty_dfs, _merge_ec_numbers and _merge_fields methods from the bioreactor main class to _utils module

--- src/biocatalyzer/_utils.py
+++ src/biocatalyzer/_utils.py
@@ -1,5 +1,8 @@
 from typing import List
 
+import numpy as np
+import pandas as pd
+
 
 def match_value(v: float, values: List[float], tol: float = 0.1) -> tuple:
     """
@@ -23,3 +26,65 @@ def match_value(v: float, values: List[float], tol: float = 0.1) -> tuple:
         if value - tol <= v <= value + tol:
             return True, i
     return False, None
+
+
+def _empty_dfs(dfs: List[pd.DataFrame]):
+    """
+    Check if at least one dataframe is not empty.
+
+    Parameters
+    ----------
+    dfs: List[pd.DataFrame]
+        The list of dataframes to check.
+
+    Returns
+    -------
+    bool:
+        True if at least one dataframe is not empty. Otherwise, False.
+    """
+    for r in dfs:
+        if not r.empty:
+            return False
+    return True
+
+
+def _merge_ec_numbers(x):
+    """
+    Merge multiple EC numbers.
+
+    Parameters
+    ----------
+    x: str
+        The EC numbers to merge.
+
+    Returns
+    -------
+    str:
+        The merged EC numbers.
+    """
+    if x == '':
+        return np.NaN
+    seen = set()
+    seen_add = seen.add
+    return ';'.join([y for y in x.split(';') if not (y in seen or seen_add(y) or y == '')])
+
+
+def _merge_fields(value):
+    """
+    Merge multiple fields.
+
+    Parameters
+    ----------
+    value: str
+        The fields to merge.
+
+    Returns
+    -------
+    str:
+        The merged fields.
+    """
+    if len(value.split(';')) == 1:
+        return value
+    seen = set()
+    seen_add = seen.add
+    return ';'.join([x for x in value.split(';') if not (x in seen or seen_add(x))])
