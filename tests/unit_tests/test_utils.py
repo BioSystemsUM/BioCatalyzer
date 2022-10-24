f29a761762b27d53e41257df51c7546b8c499a48
João Correia <jfscorreia95@gmail.com>
2022-10-24 13:46:33 +0100

    [ADD] tests for utils functions

--- tests/unit_tests/test_utils.py
+++ tests/unit_tests/test_utils.py
@@ -1,6 +1,9 @@
 from unittest import TestCase
 
-from biocatalyzer._utils import match_value
+import numpy as np
+import pandas as pd
+
+from biocatalyzer._utils import match_value, _empty_dfs, _merge_fields
 
 
 class TestUtils(TestCase):
@@ -8,3 +11,24 @@ class TestUtils(TestCase):
     def test_match_value(self):
         self.assertTrue(match_value(10, [10.1, 10.2], 0.1)[0])
         self.assertFalse(match_value(10, [10.1, 10.2], 0.01)[0])
+
+    def test_empty_dfs(self):
+        dfs = [pd.DataFrame(), pd.DataFrame()]
+        self.assertTrue(_empty_dfs(dfs))
+        dfs = [pd.DataFrame(), pd.DataFrame({'a': [1]})]
+        self.assertFalse(_empty_dfs(dfs))
+
+    def test_merge_fields(self):
+        self.assertEqual('1.1.1.1;1.1.1.2', _merge_fields('1.1.1.1;1.1.1.2;;1.1.1.1'))
+        self.assertEqual('1.1.1.1;1.1.1.2;1.1.1.3;1.1.1.4', _merge_fields('1.1.1.1;1.1.1.2;1.1.1.3;1.1.1.4'))
+        self.assertEqual('1.1.1.1;1.1.1.2;1.1.1.3;1.1.1.4',
+                         _merge_fields('1.1.1.1;1.1.1.2;1.1.1.3;1.1.1.4;1.1.1.1;1.1.1.2;1.1.1.3;1.1.1.4'))
+        self.assertTrue(np.isnan(_merge_fields(';;;')))
+
+    def test_merge_fields2(self):
+        self.assertEqual('field', _merge_fields('field'))
+        self.assertEqual('1.1.1.1;1.1.1.2;1.1.1.3;1.1.1.4', _merge_fields('1.1.1.1;1.1.1.2;1.1.1.3;1.1.1.4'))
+        self.assertEqual('1.1.1.1;1.1.1.2;1.1.1.3;1.1.1.4',
+                         _merge_fields('1.1.1.1;1.1.1.2;1.1.1.3;1.1.1.4;1.1.1.1;1.1.1.2;1.1.1.3;1.1.1.4'))
+        self.assertTrue(np.isnan(_merge_fields('')))
+
