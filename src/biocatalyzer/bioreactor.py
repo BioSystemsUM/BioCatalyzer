e9b38b8b702ff2dc121f9b2c32217cf1c2548e6c
João Correia <jfscorreia95@gmail.com>
2024-06-28 18:17:45 +0100

    [FIX] work on all OS

--- src/biocatalyzer/bioreactor.py
+++ src/biocatalyzer/bioreactor.py
@@ -605,9 +605,9 @@ class BioReactor:
                         with open(self._new_compounds_path, 'a',  newline='', encoding='utf-8') as f:
                             f.write(f"{smiles_id}\t{smiles}\t{smarts_id}\t{smiles_id}_{uuid.uuid4()}\t"
                                     f"{most_similar_product}\t{result}\t{ecs}\n")
-                            if f"{smiles_id}\t{smiles}\t{smarts_id}\t{smiles_id}_{uuid.uuid4()}\t{most_similar_product}\t{result}\t{ecs}\n".split('\t') != 6:
-                                e = f"{smiles_id}\t{smiles}\t{smarts_id}\t{smiles_id}_{uuid.uuid4()}\t{most_similar_product}\t{result}\t{ecs}\n"
-                                raise ValueError(f"Wrong number of columns. Got {len(e.split('\t'))} columns. {e}")
+                            line = f"{smiles_id}\t{smiles}\t{smarts_id}\t{smiles_id}_{uuid.uuid4()}\t{most_similar_product}\t{result}\t{ecs}\n".split('\t')
+                            if len(line) != 7:
+                                raise ValueError(f"Wrong number of columns. Got {line}")
 
 
     def react(self):
