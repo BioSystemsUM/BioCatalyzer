555c1e8cb62ba0406799ec36a501e25c045fd733
João Correia <jfscorreia95@gmail.com>
2024-06-28 17:10:07 +0100

    [FIX] work on all OS

--- src/biocatalyzer/bioreactor.py
+++ src/biocatalyzer/bioreactor.py
@@ -602,7 +602,7 @@ class BioReactor:
                         if self._neutralize:
                             most_similar_product = ChemUtils.uncharge_smiles(most_similar_product)
                         ecs = self._get_ec_numbers(smarts_id)
-                        with open(self._new_compounds_path, 'a') as f:
+                        with open(self._new_compounds_path, 'a',  newline='', encoding='utf-8') as f:
                             f.write(f"{smiles_id}\t{smiles}\t{smarts_id}\t{smiles_id}_{uuid.uuid4()}\t"
                                     f"{most_similar_product}\t{result}\t{ecs}\n")
 
